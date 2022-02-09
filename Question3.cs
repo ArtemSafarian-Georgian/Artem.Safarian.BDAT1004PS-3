a988b6104ac0159a23d7a09cb1618e489fa71fcb
ArtemSafarian-Georgian <99339968+ArtemSafarian-Georgian@users.noreply.github.com>
2022-02-09 12:48:21 -0500

    Create Question3.cs

---
+++ Question3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Question3
+{
+    class Question3
+    {
+        static void Main(string[] args){
+
+                triangleArea(2,2,2);
+            static void triangleArea(int a,int b,int c){
+
+                int s =(a+b+c)/2;
+                object area = Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+                Console.WriteLine(Convert.ToString(area));
+
+            }
+        }
+       
+        }
+
+
+}       
+        
+        
+
+       
+
+    
+
